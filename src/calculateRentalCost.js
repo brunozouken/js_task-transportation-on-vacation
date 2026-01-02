c69c22879ab4788a22799e7542226f90ca582f03
brunozouken <brunoslash59999@gmail.com>
2026-01-02 15:43:04 -0300

    Modificando a tarefa para ficar de acordo com as regras

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,16 +3,24 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
-  let total = days * 40;
+const PRICE_PER_DAY = 40;
+const LONG_RENT_DAYS = 7;
+const LONG_RENT_DISCOUNT = 50;
+const MID_RENT_DAYS = 3;
+const MID_RENT_DISCOUNT = 20;
 
-  if (days >= 7) {
-    total -= 50;
-  } else if (days >= 3) {
-    total -= 20;
+function calculateRentalCost(days) {
+  const baseCost = days * PRICE_PER_DAY;
+
+  if (days >= LONG_RENT_DAYS) {
+    return baseCost - LONG_RENT_DISCOUNT;
   }
 
-  return total;
+  if (days >= MID_RENT_DAYS) {
+    return baseCost - MID_RENT_DISCOUNT;
+  }
+
+  return baseCost;
 }
 
 module.exports = calculateRentalCost;
